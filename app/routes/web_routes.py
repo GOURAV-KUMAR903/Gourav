20a656065e10f69fb2d76c6af13e0e32d498ef21
Gourav Kumar <gni.gourav@gmail.com>
2026-05-01 16:19:36 +0530

    db connect

--- app/routes/web_routes.py
+++ app/routes/web_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.helpers.view_loader import render_view
+from app.db import cursor  # 👈 IMPORTANT
 
 router = APIRouter()
 
@@ -12,5 +13,8 @@ def home():
 @router.get("/db-test")
 def test_db():
     cursor.execute("SELECT 1")
-    return {"status": "DB connected"}
-
+    result = cursor.fetchone()
+    return {
+        "status": "DB connected",
+        "result": result
+    }
