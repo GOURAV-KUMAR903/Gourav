dc1d56cb2d8306c60571562160a6b1285c142003
Gourav Kumar <gni.gourav@gmail.com>
2026-05-02 12:38:47 +0530

    db consfdHBDHSBX

--- app/helpers/view_loader.py
+++ app/helpers/view_loader.py
@@ -3,9 +3,9 @@ from fastapi.templating import Jinja2Templates
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-templates = Jinja2Templates(directory="app/views/templates")
-
-print("TEMPLATE PATH:", os.path.join(BASE_DIR, "views", "templates"))
+templates = Jinja2Templates(
+    directory=os.path.join(BASE_DIR, "views", "templates")
+)
 
 def render_view(request, template_name: str, context: dict = None):
     context = context or {}
